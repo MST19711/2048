b03f5ce282beff68c183e3acaf53359e376374e0
Li_CX_(mobile) <13041296428@163.com>
2021-12-09 15:52:41 +0800

    完成了“A”的键盘事件响应
    注意x，y的初始化

--- 2048.cpp
+++ 2048.cpp
@@ -1,7 +1,9 @@
 #include<iostream>
 #include<time.h>
 #include<stdlib.h>
-//#include<conio.h>
+#include<conio.h>
+#include<Windows.h>
+#define KEY_DOWN(x) ((GetAsyncKeyState(x)&0x8000)?1:0)
 using namespace std;
 int M[10][10];
 int times=0;
@@ -36,6 +38,7 @@ void init(int size){
     }
 }
 void print(int size){
+    system("cls");
     for(int k=1;k<=(size*11)+1;k++) cout<<"-";
     cout<<endl;
     for(int i=1;i<=size;i++){
@@ -51,8 +54,10 @@ void print(int size){
         for(int k=1;k<=(size*11)+1;k++) cout<<"-";
         cout<<endl;
     }
+    cout<<"r:restart a game"<<endl;
 }
 void move_right(int size){
+    cout<<"right"<<endl;
     bool flag=0;
     for(int i=1;i<=size;i++){
         for(int j=size;j>=1;j--){
@@ -76,8 +81,10 @@ void move_right(int size){
         while(M[x][y]!=0)   x=(rand()%size)+1 , y=(rand()%size)+1;
         M[x][y]=rand_number();
     }
+    cout<<"right:down"<<endl;
 }
 void move_left(int size){
+    cout<<"left"<<endl;
     bool flag=0;
     for(int i=1;i<=size;i++){
         for(int j=1;j<=size;j++){
@@ -95,14 +102,19 @@ void move_left(int size){
             }
         }
     }
+    print(size);
     if(flag){
         srand(time(0)+times);
-        int x,y;
-        while(M[x][y]!=0)   x=(rand()%size)+1 , y=(rand()%size)+1;
+        int x=0,y=0;
+        cout<<x<<","<<y<<endl;
+        while(M[x][y]!=0)   x=(rand()%size)+1 , y=(rand()%size)+1,cout<<x<<","<<y<<endl;
+        cout<<"???"<<endl;
         M[x][y]=rand_number();
     }
+    cout<<"left:down"<<endl;
 }
 void move_up(int size){
+    cout<<"up"<<endl;
     bool flag=0;
     for(int i=1;i<=size;i++){
         for(int j=1;j<=size;j++){
@@ -126,8 +138,10 @@ void move_up(int size){
         while(M[x][y]!=0)   x=(rand()%size)+1 , y=(rand()%size)+1;
         M[x][y]=rand_number();
     }
+    cout<<"up:done"<<endl;
 }
 void move_down(int size){
+    cout<<"down"<<endl;
     bool flag=0;
     for(int i=size;i>=1;i--){
         for(int j=1;j<=size;j++){
@@ -151,6 +165,7 @@ void move_down(int size){
         while(M[x][y]!=0)   x=(rand()%size)+1 , y=(rand()%size)+1;
         M[x][y]=rand_number();
     }
+    cout<<"down:done"<<endl;
 }
 int main(){
     int Size=4;
@@ -158,25 +173,43 @@ int main(){
         times++;
         char o;
         system("cls");
-        cout<<"s:start"<<endl<<"e::exit"<<endl;
+        cout<<"s:start"<<endl<<"e:exit"<<endl;
         cin>>o;
         if(o=='e') break;
         init(Size);
         print(Size);
         while(1){
-            if (/*_kbhit()*/1){
-                char ch ;//= _getch();
-                cout<<"r:restart a game"<<endl;
+            if(KEY_DOWN(65)){
+                cout<<"A"<<endl;
+                move_left(Size),print(Size);//A
+                cout<<"A";
+                while(KEY_DOWN(65));
+            }
+            if(KEY_DOWN(68)){
+                move_right(Size),print(Size);//D  
+                while(KEY_DOWN(68));  
+            }
+            if(KEY_DOWN(87)){
+                move_up(Size),print(Size);//W 
+                while(KEY_DOWN(87));   
+            }
+            if(KEY_DOWN(83)){
+                move_down(Size),print(Size);//S  
+                while(KEY_DOWN(83));  
+            }
+            
+            /*if (1){   //_kbhit()){
+                char ch ;//= getchar();
                 cin>>ch;
                 if(ch=='r') {   break;  }
                 if(ch=='d') move_right(Size);
                 if(ch=='a') move_left(Size);
                 if(ch=='w') move_up(Size);
                 if(ch=='s') move_down(Size);
-                getchar();
-                system("cls");
+                //getchar();
                 print(Size);
             }
+            */
         }
     }
     return 0;
