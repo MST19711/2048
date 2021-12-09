2fc26b7ea8b93ad65d5f7e9ebdb71878f4eb7285
Li_CX_(mobile) <13041296428@163.com>
2021-12-09 14:54:04 +0800

    基本功能完成
    输入字母进行操作，没有gui，没有撤回，没有存档，没有评分，没有游戏结束条件判定

--- 2048.cpp
+++ 2048.cpp
@@ -1,9 +1,20 @@
 #include<iostream>
 #include<time.h>
 #include<stdlib.h>
+//#include<conio.h>
 using namespace std;
 int M[10][10];
 int times=0;
+bool In(int n,int size){
+    return (n>=1 and n<=size);
+}
+int next_nonzero_add(int size,int i,int j,int di,int dj){
+    int ti=i,tj=j;
+    while( M[ti][tj]==0 and In(ti,size) and In(tj,size) ) ti+=di,tj+=dj;
+    if(ti!=i)   return ti;
+    if(tj!=j)   return tj;
+    return 0;
+}
 int rand_number(){
     int num=rand()%8;
     num*=num,num*=num,num/=1000,num++;
@@ -25,7 +36,7 @@ void init(int size){
     }
 }
 void print(int size){
-    for(int k=1;k<=size*11;k++) cout<<"-";
+    for(int k=1;k<=(size*11)+1;k++) cout<<"-";
     cout<<endl;
     for(int i=1;i<=size;i++){
         cout<<"|";
@@ -37,8 +48,108 @@ void print(int size){
             cout<<M[i][j]<<"  |";
         }
         cout<<endl;
-        for(int k=1;k<=size*11;k++) cout<<"-";
+        for(int k=1;k<=(size*11)+1;k++) cout<<"-";
         cout<<endl;
+    }
+}
+void move_right(int size){
+    bool flag=0;
+    for(int i=1;i<=size;i++){
+        for(int j=size;j>=1;j--){
+            int tmp=next_nonzero_add(size,i,j,0,-1);
+            if(tmp!=0){
+                swap(M[i][j],M[i][tmp]);
+                flag=1;
+            }
+            if(M[i][j+1]==M[i][j] and j!=size and M[i][j]!=0){
+                //cout<<i<<","<<j<<" :  "<<M[i][j]<<endl;
+                flag=1;
+                M[i][j+1]*=2;
+                M[i][j]=0;
+                swap(M[i][j],M[i][next_nonzero_add(size,i,j,0,-1)]);
+            }
+        }
+    }
+    if(flag){
+        srand(time(0)+times);
+        int x,y;
+        while(M[x][y]!=0)   x=(rand()%size)+1 , y=(rand()%size)+1;
+        M[x][y]=rand_number();
+    }
+}
+void move_left(int size){
+    bool flag=0;
+    for(int i=1;i<=size;i++){
+        for(int j=1;j<=size;j++){
+            int tmp=next_nonzero_add(size,i,j,0,1);
+            if(tmp!=0 and tmp!=size+1){
+                swap(M[i][j],M[i][tmp]);
+                flag=1;
+            }
+            if(M[i][j-1]==M[i][j] and j-1!=0 and M[i][j]!=0){
+                //cout<<i<<","<<j<<" :  "<<M[i][j]<<endl;
+                flag=1;
+                M[i][j-1]*=2;
+                M[i][j]=0;
+                swap(M[i][j],M[i][next_nonzero_add(size,i,j,0,1)]);
+            }
+        }
+    }
+    if(flag){
+        srand(time(0)+times);
+        int x,y;
+        while(M[x][y]!=0)   x=(rand()%size)+1 , y=(rand()%size)+1;
+        M[x][y]=rand_number();
+    }
+}
+void move_up(int size){
+    bool flag=0;
+    for(int i=1;i<=size;i++){
+        for(int j=1;j<=size;j++){
+            int tmp=next_nonzero_add(size,i,j,1,0);
+            if(tmp!=0 and tmp!=size+1){
+                swap(M[i][j],M[tmp][j]);
+                flag=1;
+            }
+            if(M[i-1][j]==M[i][j] and i-1!=0 and M[i][j]!=0){
+                //cout<<i<<","<<j<<" :  "<<M[i][j]<<endl;
+                flag=1;
+                M[i-1][j]*=2;
+                M[i][j]=0;
+                swap(M[i][j],M[next_nonzero_add(size,i,j,1,0)][j]);
+            }
+        }
+    }
+    if(flag){
+        srand(time(0)+times);
+        int x,y;
+        while(M[x][y]!=0)   x=(rand()%size)+1 , y=(rand()%size)+1;
+        M[x][y]=rand_number();
+    }
+}
+void move_down(int size){
+    bool flag=0;
+    for(int i=size;i>=1;i--){
+        for(int j=1;j<=size;j++){
+            int tmp=next_nonzero_add(size,i,j,-1,0);
+            if(tmp!=0 and tmp!=size+1){
+                swap(M[i][j],M[tmp][j]);
+                flag=1;
+            }
+            if(M[i+1][j]==M[i][j] and i+1!=0 and M[i][j]!=0){
+                //cout<<i<<","<<j<<" :  "<<M[i][j]<<endl;
+                flag=1;
+                M[i+1][j]*=2;
+                M[i][j]=0;
+                swap(M[i][j],M[next_nonzero_add(size,i,j,-1,0)][j]);
+            }
+        }
+    }
+    if(flag){
+        srand(time(0)+times);
+        int x,y;
+        while(M[x][y]!=0)   x=(rand()%size)+1 , y=(rand()%size)+1;
+        M[x][y]=rand_number();
     }
 }
 int main(){
@@ -46,10 +157,27 @@ int main(){
     while(1){
         times++;
         char o;
+        system("cls");
+        cout<<"s:start"<<endl<<"e::exit"<<endl;
         cin>>o;
         if(o=='e') break;
         init(Size);
         print(Size);
+        while(1){
+            if (/*_kbhit()*/1){
+                char ch ;//= _getch();
+                cout<<"r:restart a game"<<endl;
+                cin>>ch;
+                if(ch=='r') {   break;  }
+                if(ch=='d') move_right(Size);
+                if(ch=='a') move_left(Size);
+                if(ch=='w') move_up(Size);
+                if(ch=='s') move_down(Size);
+                getchar();
+                system("cls");
+                print(Size);
+            }
+        }
     }
     return 0;
 }
